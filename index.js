b61f8693748f07508c3e714e8e75003b063f163a
Simeaux <50959449+Simeaux@users.noreply.github.com>
2026-09-25 12:38:37 +0200

    fix: caricamento diretto chiavi in formato PEM nativo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path");
 async function createPass() {
   try {
     const base64Cert = process.env.APPLE_PASS_CERT; 
-    const passphrase = process.env.APPLE_PASS_PASSWORD;
+    const passphrase = process.env.APPLE_PASS_KEY;
     let rawWwdr = process.env.APPLE_WWDR_CERT;
 
     if (!base64Cert || !passphrase || !rawWwdr) {
